cd8ad0cb698180c663311fafa14908bc2166a89c
Husna Amalia <amalia.husna.ft19@mail.umy.ac.id>
2021-03-10 13:47:11 +0700

    Amalia Husna 20190140096

--- app/src/main/java/com/example/tugas1/MainActivity.java
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.tugas1;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText edemail, edpassword, edusername;
+    String nama, password,username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        btnLogin=findViewById(R.id.button);
+        edemail=findViewById(R.id.editTextTextEmailAddress);
+        edpassword=findViewById(R.id.editTextNumberPassword);
+        edusername=findViewById(R.id.editTextTextPersonName);
+
+    btnLogin.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            nama = edemail.getText().toString();
+            password = edpassword.getText().toString();
+            username = edusername.getText().toString();
+            String username="husna";
+            String email="admin@mail.com";
+            String pass = "123";
+            if (nama.equals(email) && password.equals(pass) && username.equals(username))
+            {
+                Toast t = Toast.makeText(getApplicationContext(),
+                        "Login Berhasil", Toast.LENGTH_LONG);
+                t.show();
+            }
+            else
+            {
+                Toast t = Toast.makeText(getApplicationContext(),
+                        "Login Gagal", Toast.LENGTH_LONG);
+                t.show();
+            }
+        }
+    });
+}}
