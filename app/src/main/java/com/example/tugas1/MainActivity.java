f8983289a5888664f6a5a28b51cd2b00ac221a1a
Husna Amalia <amalia.husna.ft19@mail.umy.ac.id>
2021-03-25 10:25:08 +0700

    Activity 3 C

--- app/src/main/java/com/example/tugas1/MainActivity.java
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,42 +19,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnLogin=findViewById(R.id.button);
-        edemail=findViewById(R.id.editTextTextEmailAddress);
-        edpassword=findViewById(R.id.editTextNumberPassword);
-        edusername=findViewById(R.id.editTextTextPersonName);
+        btnLogin = findViewById(R.id.button);
+        edemail = findViewById(R.id.editTextTextEmailAddress);
+        edpassword = findViewById(R.id.editTextNumberPassword);
+        edusername = findViewById(R.id.editTextTextPersonName);
 
-    btnLogin.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View view) {
-            nama = edemail.getText().toString();
-            password = edpassword.getText().toString();
-            username = edusername.getText().toString();
-            String username="husna";
-            String email="admin@mail.com";
-            String pass = "123";
-            if (nama.equals(email) && password.equals(pass) && username.equals(username))
-            {
-                Toast t = Toast.makeText(getApplicationContext(),
-                        "Login Berhasil", Toast.LENGTH_LONG);
-                t.show();
-            }
-            else if (nama.equals(email) || password.equals(pass)) {
-                if (nama.equals(email)) {
-                    Toast ps = Toast.makeText(getApplicationContext(),
-                            "Invalid Password", Toast.LENGTH_SHORT);
-                    ps.show();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+                username = edusername.getText().toString();
+                String username = "husna";
+                String email = "admin@mail.com";
+                String pass = "123";
+                if (nama.isEmpty() || password.isEmpty()) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email dan password wajib diisi!!!", Toast.LENGTH_LONG);
+                    t.show();
+                } else {
+                    if (nama.equals(email) && password.equals(pass)) {
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Sukses", Toast.LENGTH_SHORT);
+                        t.show();
+                        Bundle b = new Bundle();
+                        b.putString("a", nama.trim());
+                        b.putString("b", password.trim());
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+                        i.putExtras(b);
+                        startActivity(i);
+                    } else {
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Gagal", Toast.LENGTH_LONG);
+                        t.show();
+                    }
                 }
-                else{
-                    Toast es = Toast.makeText(getApplicationContext(),
-                            "Invalid Email", Toast.LENGTH_LONG);
-                    es.show();
-                }}
-            else{
-                Toast t = Toast.makeText(getApplicationContext(),
-                        "Login Gagal", Toast.LENGTH_LONG);
-                t.show();
             }
-        }
-    });
-}}
+        });
+    }}
